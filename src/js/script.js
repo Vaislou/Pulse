1e15480dd407099a7a2bd95f9f17473ccb317037
Alex <vaislou@gmail.com>
2021-02-10 00:43:02 +0300

    3.12 complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,17 @@
-let number = 7;
-const pi = 3.14;
-
-number = 4;
-
-let leftBorderWidth = 200;
-
-let obj = {
-    name: 'apple',
-    color: 'green',
-    weight: 200
-}
-
-console.log (number)
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
+  });
